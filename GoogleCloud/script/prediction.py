80e30546caa4797a7d1ec28a4091444164095831
Brendan Guillouet <brendan.guillouet@gmail.com>
2018-11-12 18:34:48 +0100

    V1.141 add TODO in prediction.py

--- GoogleCloud/script/prediction.py
+++ GoogleCloud/script/prediction.py
@@ -6,8 +6,8 @@ import hashlib
 import numpy as np
 import pandas as pd
 
-import keras.preprocessing.image as kpi
-import keras.models as km
+import tensorflow.keras.preprocessing.image as kpi
+import tensorflow.keras.models as km
 
 
 from tensorflow.python.client import device_lib
@@ -18,52 +18,43 @@ print(MODE)
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--epochs', type=int, default=10)
-parser.add_argument('--batch_size', type=int, default=20)
+# TODO Les paramètres de l'environnement sont défini ici. Ajoutez les directions de vos dossier en local par défault.
 
-parser.add_argument('--data_dir', type=str,
-                    default="/Users/bguillouet/Insa/TP_Insa/dev/IA-Frameworks/tp_google_cloud/data")
-parser.add_argument('--results_dir', type=str,
-                    default="/Users/bguillouet/Insa/TP_Insa/dev/IA-Frameworks/tp_google_cloud/results")
-parser.add_argument('--model_dir', type=str,
-                    default="/Users/bguillouet/Insa/TP_Insa/dev/IA-Frameworks/tp_google_cloud/model")
+
+parser.add_argument('--data_dir', type=str, default="")
+parser.add_argument('--results_dir', type=str,  default="")
+parser.add_argument('--model_dir', type=str, default="")
+
+# TODO ajoutez ici les différents paramètres du modèle que vous souhaitez pour manipuler en entré de script
+parser.add_argument('--', type=str, default="")
 
 args = parser.parse_args()
 
-## Définition des variables
+## Definition des variables
 
 img_width = 150
 img_height = 150
 
 ## Data Generator
 
-data_dir_test = args.data_dir+'/test'
-N_test = len(os.listdir(data_dir_test+"/test"))
+# TODO définissez ici le différents "generator" qui vous permettra de lire les données et généraler les batchs
+N_test =
 
-test_datagen = kpi.ImageDataGenerator(rescale=1. / 255)
-test_generator = test_datagen.flow_from_directory(
-    data_dir_test,
-    target_size=(img_height, img_width),
-    batch_size=args.batch_size,
-    class_mode=None,
-    shuffle=False)
+test_datagen = kpi.ImageDataGenerator()
+test_generator = test_datagen.flow_from_directory(e)
 
-## Téléchargement du modèle
-args_str = "_".join([k + ":" + str(v) for k, v in vars(args).items()])
+
+## Telechargement du modele
+args_str = "_".join([k + ":" + str(v) for k, v in sorted(vars(args).items(), key=lambda x : x[0])])
 id_str = hashlib.md5(args_str.encode("utf8")).hexdigest()
 model_conv = km.load_model(args.model_dir + "/" + id_str + ".h5")
 
 
 
-## Prédiction
+## Prediction
+# TODO Effectuez la prédiction
 
-test_prediction = model_conv.predict_generator(test_generator, N_test // args.batch_size, verbose=1)
+test_prediction = model_conv.predict_generator()
 
 ## Save prediction in csv
-
-images_test = [k for k in os.listdir(data_dir_test+"/test")]
-classes = [int(t>0.5) for t in test_prediction]
-
-array = np.vstack((images_test, test_prediction[:,0], classes)).T
-df = pd.DataFrame(array, columns=["filename","probabilities","classes"])
-df.to_csv(args.results_dir+"/prediction_"+id_str+".csv", index=False)
+# TODO Sauvegarder vos résultats dans le dossier results_dir sous forme d'un fichier csv.
