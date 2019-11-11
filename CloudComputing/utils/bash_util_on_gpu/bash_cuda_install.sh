776f64cd332f085aba63bfb8782804f6a5de07d9
bguillouet <brendan.guillouet@gmail.com>
2019-11-11 17:03:03 +0100

    Update Script instalation first part

--- CloudComputing/utils/bash_util_on_gpu/bash_cuda_install.sh
+++ CloudComputing/utils/bash_util_on_gpu/bash_cuda_install.sh
@@ -1,33 +1,25 @@
+# COMMAND LINE COME FROM TENSORFLOW OFFICIAL INSTALATION GUIDE : https://www.tensorflow.org/install/gpu
 
-#https://gist.github.com/ashokpant/5c4e9481615f54af4025ab2085f85869
-
-#!/bin/bash
-
-# install CUDA Toolkit v9.0
-# instructions from https://developer.nvidia.com/cuda-downloads (linux -> x86_64 -> Ubuntu -> 16.04 -> deb)
-CUDA_REPO_PKG="cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64-deb"
-wget https://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/${CUDA_REPO_PKG}
-sudo dpkg -i ${CUDA_REPO_PKG}
-sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
+# Add NVIDIA package repositories
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
 sudo apt-get update
-sudo apt-get -y install cuda-9-0
-
-CUDA_PATCH1="cuda-repo-ubuntu1604-9-0-local-cublas-performance-update_1.0-1_amd64-deb"
-wget https://developer.nvidia.com/compute/cuda/9.0/Prod/patches/1/${CUDA_PATCH1}
-sudo dpkg -i ${CUDA_PATCH1}
+wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64/nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
+sudo apt install ./nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
 sudo apt-get update
 
-# install cuDNN v7.0
-CUDNN_PKG="libcudnn7_7.0.5.15-1+cuda9.0_amd64.deb"
-wget https://github.com/ashokpant/cudnn_archive/raw/master/v7.0/${CUDNN_PKG}
-sudo dpkg -i ${CUDNN_PKG}
-sudo apt-get update
+# Install NVIDIA driver
+sudo apt-get install --no-install-recommends nvidia-driver-418
+# Reboot. Check that GPUs are visible using the command: nvidia-smi
 
-# install NVIDIA CUDA Profile Tools Interface ( libcupti-dev v9.0)
-sudo apt-get install cuda-command-line-tools-9-0
+# Install development and runtime libraries (~4GB)
+sudo apt-get install --no-install-recommends \
+    cuda-10-0 \
+    libcudnn7=7.6.2.24-1+cuda10.0  \
+    libcudnn7-dev=7.6.2.24-1+cuda10.0
 
-# set environment variables
-export PATH=${PATH}:/usr/local/cuda-9.0/bin
-export CUDA_HOME=${CUDA_HOME}:/usr/local/cuda:/usr/local/cuda-9.0
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/cuda-9.0/lib64
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/extras/CUPTI/lib64
+
+# Install TensorRT. Requires that libcudnn7 is installed above.
+sudo apt-get install -y --no-install-recommends libnvinfer5=5.1.5-1+cuda10.0 \
+    libnvinfer-dev=5.1.5-1+cuda10.0
