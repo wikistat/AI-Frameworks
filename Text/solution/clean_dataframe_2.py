9c8be84a9b5613d3244e3d2f2b4bdeeb2baf54f9
bguillouet <brendan.guillouet@gmail.com>
2020-11-01 16:56:13 +0100

    update wget for google colab

--- Text/solution/clean_dataframe_2.py
+++ Text/solution/clean_dataframe_2.py
@@ -1,3 +1,2 @@
 def clean_df_column(df, column_name, clean_column_name):
-    nb_line = df.shape[0]
     df[clean_column_name] = [" ".join(apply_all_transformation(x)) for x in tqdm(df[column_name].values)]
