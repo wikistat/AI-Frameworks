7d902feb7f8619430d328dc3b7b7b824d8f15d49
bguillouet <brendan.guillouet@gmail.com>
2020-11-16 09:00:56 +0100

    correction exo rnn

--- Text/solution/one_to_many_prediction.py
+++ Text/solution/one_to_many_prediction.py
@@ -1,10 +1,10 @@
 def predict_function(x):
     x_test=np.array(x).reshape(1,1,1)
     y1 = model.predict(x_test)
-    y2 = model.predict(y1)
-    y3 = model.predict(y2)
-    return [y1,y2,y3]
+    y2 = model.predict(np.hstack((x_test,y1)))
+    y3 = model.predict(np.hstack((x_test,y2)))
+    return y3
 
 x=10
 y=predict_function(x)
-print("Input scalar : %d. Output sequences: [%.3f, %.3f, %.3f]" %(x, y[0],y[1],y[2]) )
+print("Input scalar : %d. Output sequences: [%.3f, %.3f, %.3f]" %(x, y[0][0],y[0][1][0],y[0][2][0]) )
